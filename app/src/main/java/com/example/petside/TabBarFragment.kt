742f4039381e6f1890a7db34b22cfb49c00422f3
Ilya <ipetrov@mmtr.ru>
2023-04-20 15:00:32 +0300

    Добавлены переходы для таб бара

--- app/src/main/java/com/example/petside/TabBarFragment.kt
+++ app/src/main/java/com/example/petside/TabBarFragment.kt
@@ -1,59 +1,53 @@
 package com.example.petside
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.petside.databinding.FragmentAuthBinding
+import com.example.petside.databinding.FragmentTabBarBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TabBarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TabBarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentTabBarBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_bar, container, false)
+
+        _binding = FragmentTabBarBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val feedFragment=FeedFragment()
+        val addPostFragment=FeedFragment()
+        val favouritesFragment=FavouritesFragment()
+        val menuFragment=FeedFragment()
+
+        setCurrentFragment(feedFragment)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.feedTab->setCurrentFragment(feedFragment)
+                R.id.addPostTab->setCurrentFragment(addPostFragment)
+                R.id.favouritesTab->setCurrentFragment(favouritesFragment)
+                R.id.menuTab->setCurrentFragment(menuFragment)
+            }
+            true
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TabBarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TabBarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setCurrentFragment(fragment:Fragment)=
+        parentFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
