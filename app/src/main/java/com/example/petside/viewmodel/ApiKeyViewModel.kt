7dce65dd9db9da0d75bcc51c8492b63ffa844204
Ilya <ipetrov@mmtr.ru>
2023-04-28 21:07:35 +0300

    Рефактор ApiKeyFragment.kt

--- app/src/main/java/com/example/petside/viewmodel/ApiKeyViewModel.kt
+++ app/src/main/java/com/example/petside/viewmodel/ApiKeyViewModel.kt
@@ -22,11 +22,11 @@ class ApiKeyViewModel : ViewModel() {
     fun checkKey(onSuccess: () -> Unit, onError: (e: HttpException) -> Unit) {
         viewModelScope.launch {
             try {
+                loading.value = true
                 retrofitService.getFavourites(newUser.api_key)
                 CoroutineScope(Dispatchers.IO).launch {
                     dao.insertUser(newUser)
                 }
-
             } catch (e: HttpException) {
                 onError(e)
                 throw CancellationException()
@@ -35,6 +35,7 @@ class ApiKeyViewModel : ViewModel() {
             if (it === null) {
                 onSuccess()
             }
+            loading.value = false
         }
     }
 
